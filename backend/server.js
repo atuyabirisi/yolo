bed509dadd7056574eea9a35066513b3d68ccc17
atuyabirisi <tushhamphrey@gmail.com>
2025-10-10 13:07:36 +0300

    add custom docker network and added services to it

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,12 @@ app.use(express.json());
 app.use(upload.array());
 
 // Cors
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 // Use Route
 app.use("/api/products", productRoute);
